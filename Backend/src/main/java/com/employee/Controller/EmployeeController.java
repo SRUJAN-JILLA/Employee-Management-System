c91d06171c75381e53cf5c85f07d170774d45444
SRUJAN-JILLA <srujanjilla15@gmail.com>
2023-01-14 07:47:34 +0530

    Export in PDF, Excel, CSV Format

--- Backend/src/main/java/com/employee/Controller/EmployeeController.java
+++ Backend/src/main/java/com/employee/Controller/EmployeeController.java
@@ -1,6 +1,12 @@
 package com.employee.Controller;
 
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,17 +17,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
 
 import com.employee.Model.Employee;
+import com.employee.Service.EmployeeExcelExporter;
+import com.employee.Service.EmployeePdfExporter;
 import com.employee.Service.EmployeeService;
+import com.lowagie.text.DocumentException;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class EmployeeController {
-	
+
 	@Autowired
 	private EmployeeService employeeService;
-	
+
 	@GetMapping("/employees")
 	public List<Employee> findAll() {
 		return this.employeeService.findAll();
@@ -37,13 +49,77 @@ public class EmployeeController {
 		this.employeeService.updateEmployee(id, employeeDetails);
 	}
 
-	@GetMapping("employees/emailExists/{email}")
+	@GetMapping("/employees/emailExists/{email}")
 	public boolean emailExists(@PathVariable("email") String email) {
 		return employeeService.findByEmail(email);
 	}
+
+	// https://www.codejava.net/frameworks/spring-boot/export-data-to-excel-example
+	@GetMapping("/employees/downloadExcel")
+	public void downloadExcel(HttpServletResponse response) throws IOException {
+		response.setContentType("application/octet-stream");
+		DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+		String currentDateTime = dateFormatter.format(new Date());
+
+		String headerKey = "Content-Disposition";
+		String headerValue = "attachment; filename=Employees_" + currentDateTime + ".xlsx";
+		response.setHeader(headerKey, headerValue);
+
+		List<Employee> listEmployee = this.employeeService.findAll();
+
+		EmployeeExcelExporter employeeExcelExporter = new EmployeeExcelExporter(listEmployee);
+
+		employeeExcelExporter.export(response);
+	}
 	
+	//https://www.codejava.net/frameworks/spring-boot/pdf-export-example
+	@GetMapping("/employees/downloadPdf")
+	public void downloadPdf(HttpServletResponse response) throws DocumentException, IOException {
+
+		response.setContentType("application/pdf");
+		DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+		String currentDateTime = dateFormatter.format(new Date());
+
+		String headerKey = "Content-Disposition";
+		String headerValue = "attachment; filename=Employees_" + currentDateTime + ".pdf";
+		response.setHeader(headerKey, headerValue);
+
+		List<Employee> listEmployee = this.employeeService.findAll();
+
+		EmployeePdfExporter exporter = new EmployeePdfExporter(listEmployee);
+		exporter.export(response);
+
+	}
+
+	//https://www.codejava.net/frameworks/spring-boot/csv-export-example
+	@GetMapping("/employees/downloadCsv")
+	public void downloadCsv(HttpServletResponse response) throws IOException {
+		response.setContentType("text/csv");
+		DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+		String currentDateTime = dateFormatter.format(new Date());
+
+		String headerKey = "Content-Disposition";
+		String headerValue = "attachment; filename=Employees_" + currentDateTime + ".csv";
+		response.setHeader(headerKey, headerValue);
+
+		List<Employee> listEmployee = this.employeeService.findAll();
+
+		ICsvBeanWriter csvWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
+		String[] csvHeader = { "ID", "First Name", "Last Name", "E-mail", "Salary", "Job" };
+		String[] nameMapping = { "id", "firstName", "lastName", "email", "salary", "job" };
+
+		csvWriter.writeHeader(csvHeader);
+
+		for (Employee employee : listEmployee) {
+			csvWriter.write(employee, nameMapping);
+		}
+
+		csvWriter.close();
+
+	}
+
 	// Extra
-	
+
 	@GetMapping("/employees/{id}")
 	public Employee findById(@PathVariable("id") long id) {
 		return this.employeeService.findById(id);
@@ -53,12 +129,11 @@ public class EmployeeController {
 	public String addEmployee(@RequestBody Employee employee) {
 		return employeeService.addEmployee(employee);
 	}
-	
+
 	@DeleteMapping("employees")
 	public void deleteAll() {
 		employeeService.deleteAll();
 	}
-	
 
 	// Another way of fetching data using Mongo Template
 	// @Autowired
@@ -66,7 +141,7 @@ public class EmployeeController {
 	//
 	// @GetMapping("/employees")
 	// public List<Employee> getAllEmployees() {
-	//	
+	//
 	// query.addCriteria(Criteria.where("salary").gt(0));
 	// return mongoTemplate.find(query, Employee.class);
 	// }
